cc8afd3a644cfb151d7c2ec5c643bdbef38523b8
AvilaJoseph <avilajoseph2021@gmail.com>
2025-02-24 13:11:19 -0500

    subrutas y estructura layouts de country

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutsComponent } from './layouts/CountryLayouts/CountryLayouts.component';
 
 export const countryRoutes: Routes = [
 
     {
         path: '',
-        component: ByCapitalPageComponent
+        component: CountryLayoutsComponent,
+        children: [
+            {
+                path:'by-capital',
+                component: ByCapitalPageComponent
+            }
+        ]
     }
 
 ];
---
+++ src/app/country/layouts/CountryLayouts/CountryLayouts.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-country-layouts',
+  imports: [RouterOutlet],
+  templateUrl: './CountryLayouts.component.html',
+})
+export class CountryLayoutsComponent { }

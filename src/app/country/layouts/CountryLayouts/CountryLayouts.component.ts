1c3b3d42183c9ea53dab0ba8ae9c6844b4a894af
AvilaJoseph <avilajoseph2021@gmail.com>
2025-02-25 23:32:06 -0500

    creación del navbar

--- src/app/country/layouts/CountryLayouts/CountryLayouts.component.ts
+++ src/app/country/layouts/CountryLayouts/CountryLayouts.component.ts
@@ -1,9 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from '../../components/top-menu/top-menu.component';
 
 @Component({
   selector: 'app-country-layouts',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    TopMenuComponent
+  ],
   templateUrl: './CountryLayouts.component.html',
 })
 export class CountryLayoutsComponent { }

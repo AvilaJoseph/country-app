613c058465eabaadb4811d33d2c252124eb6b32d
AvilaJoseph <avilajoseph2021@gmail.com>
2025-02-28 23:43:27 -0500

    Implementacion de mostrar errores

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,11 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-country.interface';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, throwError } from 'rxjs';
 import { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
-const API_URL='https://restcountries.com/v3.1'
+const API_URL = 'https://restcountries.com/v3.1'
 
 @Injectable({
   providedIn: 'root'
@@ -18,9 +18,15 @@ export class CountryService {
   searchByCapital(query: string): Observable<Country[]> {
     query = query.toLowerCase();
     return this.http.get<RESTCountry[]>(`${API_URL}/capital/${query}`)
-    .pipe(
-      map(restCountries => CountryMapper.mapRestCountrytoCountryArray(restCountries) )
-    )
-  }
-  
+      .pipe(
+        map(restCountries => CountryMapper.mapRestCountrytoCountryArray(restCountries)),
+        catchError(error => {
+          console.log('Error fetching:', error);
+          return throwError(() => 
+            new Error(`No se encontraron resultados con esta capital: ${query}`)
+          );
+        })
+      );
+  }  
+
 }

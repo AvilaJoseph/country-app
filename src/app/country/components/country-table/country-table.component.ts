0750d0f1cff4acf768ccb166a1873d0839df983b
AvilaJoseph <avilajoseph2021@gmail.com>
2025-03-01 22:36:44 -0500

    implementación de ruta dinamica

--- src/app/country/components/country-table/country-table.component.ts
+++ src/app/country/components/country-table/country-table.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'country-table',
   standalone: true,
   imports: [
     CommonModule,
+    RouterLink
   ],
   templateUrl: './country-table.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,

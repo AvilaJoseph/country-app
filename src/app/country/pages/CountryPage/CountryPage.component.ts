41b5aac58cfee325de006a67fc36f437e9df09b9
AvilaJoseph <avilajoseph2021@gmail.com>
2025-03-02 23:41:06 -0500

    creación de metodo de busqueda

--- src/app/country/pages/CountryPage/CountryPage.component.ts
+++ src/app/country/pages/CountryPage/CountryPage.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'country-page',
@@ -14,5 +16,12 @@ import { ActivatedRoute } from '@angular/router';
 export class CountryPageComponent {
 
   CountryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryServices = inject(CountryService);
+  countryResources = rxResource({
+    request: () => ({ code: this.CountryCode }),
+    loader: ({ request }) =>{
+      return this.countryServices.searchCountrybyAlphaCode(request.code)
+    }
+  })
 
  }
